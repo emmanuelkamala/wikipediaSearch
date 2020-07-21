34cfab158d4b949907ee8958d0b554111024c687
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-21 14:35:45 +0300

    Create a search form event listener

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,19 @@ const feedback = document.querySelector(".feedback");
 const base = "https://en.wikipedia.org/w/api.php";
 const url = "?action=query&format=json&origin=*&list=search&srsearch=";
 
-
-
+searchForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    const value = search.value;
+  
+    if (value === "") {
+      showFeedback("please enter a valid search value");
+    } else {
+      search.value = "";
+      // ajax
+      ajaxWiki(value);
+    }
+  });
 
 
 //show feedback
