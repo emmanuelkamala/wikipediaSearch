d8212f2a2fa5699ab4dccbd7047d7dfd860b88b4
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-21 14:30:20 +0300

    Configure webpack in the app

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,10 @@ const output = document.querySelector(".output");
 const search = document.getElementById("search");
 const feedback = document.querySelector(".feedback");
 
+const base = "https://en.wikipedia.org/w/api.php";
+const url = "?action=query&format=json&origin=*&list=search&srsearch=";
+
+
 
 
 
