15b5a8f6ee67722ae6573202abcad1bf2438a35f
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-21 14:12:22 +0300

    Select html elements in JS file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 //select elements
-
+const loading = document.querySelector(".loading");
+const searchForm = document.getElementById("searchForm");
+const output = document.querySelector(".output");
+const search = document.getElementById("search");
+const feedback = document.querySelector(".feedback");
 
 
 
